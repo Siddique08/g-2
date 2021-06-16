ab5a380f12324fbe1d94a2596d88dda99c60053d
nomanusa <38590366+nomanusa@users.noreply.github.com>
2021-06-16 10:46:19 -0400

    adding handling Keybord and mouse

---
+++ src/main/java/g_2/HandlingKeyboardandMouse.java
@@ -0,0 +1,33 @@
+package g_2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HandlingKeyboardandMouse {
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Noman\\Desktop\\QA Class\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/");
+		driver.manage().window().maximize();
+		
+		
+		Actions act = new Actions(driver);
+		driver.findElement(By.xpath("//a[text()='Tooltip']")).click();
+		driver.switchTo().frame(0);
+		
+		WebElement ttip = driver.findElement(By.xpath("//a[text()='Tooltip']"));
+		WebElement trol = driver.findElement(By.xpath("//a[text()='ThemeRoller']"));
+		
+		act.moveToElement(ttip).moveToElement(trol).build().perform();
+		
+		driver.switchTo().defaultContent();
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+
+}

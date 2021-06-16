6c8560aa9fa117096d2f39305aeb2f5eb67db9f2
nomanusa <38590366+nomanusa@users.noreply.github.com>
2021-06-16 11:23:09 -0400

    Adding pause(3000)

--- src/main/java/g_2/HandlingKeyboardAndMouse.java
+++ src/main/java/g_2/HandlingKeyboardAndMouse.java
@@ -1,5 +1,7 @@
 package g_2;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,19 +10,21 @@ import org.openqa.selenium.interactions.Actions;
 
 public class HandlingKeyboardAndMouse {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Noman\\Desktop\\QA Class\\Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://jqueryui.com/");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.NANOSECONDS);
 		
 		
+		driver.findElement(By.xpath("//a[text()='Tooltip']")).click();
 		Actions act = new Actions(driver);
-		driver.findElement(By.xpath("//a[text()='Tooltips']")).click();
+		
 		driver.switchTo().frame(0);
 		WebElement ttip = driver.findElement(By.xpath("//a[text()='Tooltips']"));
 		WebElement trol = driver.findElement(By.xpath("//a[text()='ThemeRoller']"));
-		act.moveToElement(ttip).moveToElement(trol).build().perform();
+		act.moveToElement(ttip).pause(3000).moveToElement(trol).build().perform();
 		driver.switchTo().defaultContent();
 		Thread.sleep(5000);
 		driver.close();

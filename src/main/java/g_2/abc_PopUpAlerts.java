a957e0a21a1341c7b284a0e33c26223ed3790282
mabs0 <mabs0@LAPTOP-Q1BLTQ09.myfiosgateway.com>
2021-06-25 11:05:48 -0400

    con

--- src/main/java/g_2/abc_PopUpAlerts.java
+++ src/main/java/g_2/abc_PopUpAlerts.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class abc_PopUpAlerts {
 
-	
+	// ckkjj - abc
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver_91.exe");  
 	    WebDriver driver = new ChromeDriver();

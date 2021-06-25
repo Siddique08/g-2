da3550f7b9427c3f277647b1e5a3c010282895e2
Noman Siddiki <38590366+nomanusa@users.noreply.github.com>
2021-06-25 11:08:37 -0400

    Update abc_PopUpAlerts.java

--- src/main/java/g_2/abc_PopUpAlerts.java
+++ src/main/java/g_2/abc_PopUpAlerts.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class abc_PopUpAlerts {
 
 	// ckkjj - abc
+	//Farhan
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver_91.exe");  
 	    WebDriver driver = new ChromeDriver();
